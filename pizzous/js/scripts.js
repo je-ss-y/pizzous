fa9d396b0df9febbb3dec1ce42eba90c54b57f6e
je-ss-y <nkujess@gmail.com>
2019-08-05 13:54:43 +0200

    styled my header allignments

--- pizzous/js/scripts.js
+++ pizzous/js/scripts.js
@@ -35,23 +35,22 @@ var deliveryPrice=0;
 function pizza(){
        
      
-            // var valueadd =$("input[name=yes]:checked").val();
-            // if(valueadd==="yes"){
-            //     deliveryPrice= 500;
-            //     var getLocation = prompt("Tell your Location:");
-            //     alert("Will be delivered at" + getLocation);
-            // }
+            var valueadd =$("input[name=yes]:checked").val();
+            if(valueadd==="yes"){
+                deliveryPrice= 500;
+                var getLocation = prompt("Tell your Location:");
+                alert("Will be delivered at" + getLocation);
+            }
 
         
 
   
     var sizechoice=parsefloat(document.getElementById('pizzasize').value);
     var crustchoice=parsefloat(document.getElementById('pizzacrust').value);
-    // var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
+    var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
 
     var total= sizechoice + crustchoice + deliveryPrice;
     document.getElementById("totalamount").innerHTML =total;
-    // event.preventDefault();
     
 
 };
