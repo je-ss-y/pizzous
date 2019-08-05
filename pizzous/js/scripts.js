5645ca7f4bb7abfb342403cfadc93d8ea9600412
je-ss-y <nkujess@gmail.com>
2019-08-05 14:27:03 +0200

    added history to be displayed

--- pizzous/js/scripts.js
+++ pizzous/js/scripts.js
@@ -32,21 +32,26 @@
 
 
 var deliveryPrice=0;
-// function orders(){
-//     var valueadd =$("input[name=yes]:checked").val();
-//             if(valueadd==="yes"){
-//                 deliveryPrice= 500;
-//                 var getLocation = prompt("Tell your Location:");
-//                 alert("Will be delivered at" + getLocation);
+function orders(){
+    var valueadd =parsefloat(document.getElementById('myinputs').value);
+            if(valueadd==="yes"){
+                deliveryPrice= 500;
+                var getLocation = prompt("Tell your Location:");
+                alert("Will be delivered at" + getLocation);
             
-// }
+}
 function pizza(){
     var sizechoice=parsefloat(document.getElementById('pizzasize').value);
     var crustchoice=parsefloat(document.getElementById('pizzacrust').value);
     var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
 
-    var total= sizechoice + crustchoice + toppingschoice+deliveryPrice;
+    var total= (sizechoice + crustchoice + toppingschoice+deliveryPrice);
+    document.getElementById("sizehistory").innerHTML = sizechoice;
+    document.getElementById("crusthistory").innerHTML = crustchoice;
+    document.getElementById("toppingshistory").innerHTML = toppingschoice;
+
     document.getElementById("totalamount").innerHTML =total;
     
 
+}
 }
