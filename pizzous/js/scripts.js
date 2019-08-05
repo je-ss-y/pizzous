ae016e352fa990d2bd5a368f5bb03cc825cc508f
je-ss-y <nkujess@gmail.com>
2019-08-05 14:18:09 +0200

    changed my function

--- pizzous/js/scripts.js
+++ pizzous/js/scripts.js
@@ -32,25 +32,21 @@
 
 
 var deliveryPrice=0;
+// function orders(){
+//     var valueadd =$("input[name=yes]:checked").val();
+//             if(valueadd==="yes"){
+//                 deliveryPrice= 500;
+//                 var getLocation = prompt("Tell your Location:");
+//                 alert("Will be delivered at" + getLocation);
+            
+// }
 function pizza(){
-       
-     
-            var valueadd =$("input[name=yes]:checked").val();
-            if(valueadd==="yes"){
-                deliveryPrice= 500;
-                var getLocation = prompt("Tell your Location:");
-                alert("Will be delivered at" + getLocation);
-            }
-
-        
-
-  
     var sizechoice=parsefloat(document.getElementById('pizzasize').value);
     var crustchoice=parsefloat(document.getElementById('pizzacrust').value);
     var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
 
-    var total= sizechoice + crustchoice + deliveryPrice;
+    var total= sizechoice + crustchoice + toppingschoice+deliveryPrice;
     document.getElementById("totalamount").innerHTML =total;
     
 
-};
+}
