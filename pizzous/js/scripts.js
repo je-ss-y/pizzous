6b5096c813114e7d39c6cdefb932c3ce7559ff79
je-ss-y <nkujess@gmail.com>
2019-08-05 13:47:30 +0200

    changed my jquery to a function only

--- pizzous/js/scripts.js
+++ pizzous/js/scripts.js
@@ -1,37 +1,57 @@
-// //business logic//
-// function order(size,crust,quantity,delivery){
-//        this.pizzasize=size;
-//        this.pizzacrust=crust;
-//        this.pizzaquantity=quantity;
-// }
-// order.prototype.allfunction(){
-// return this.pizzasize + ";" +this.pizzacrust + ";"+ this.pizzaquantity + ";" ;
-// }
-// //user interface//
-// $(document).ready(function){
-//      var 
 
-// }
-$(document). ready(function(){
-        var deliveryPrice=0;
-        $("#order").click(function(){
-            var valueadd =$("input[name=yes]:checked").val();
-            if(valueadd==="yes"){
-                deliveryPrice= 500;
-                var getLocation = prompt("Tell your Location:");
-                alert("Will be delivered at" + getLocation);
-            }
+// $(document). ready(function(){
+//         var deliveryPrice=0;
+//         $("#order").click(function(){
+//             var valueadd =$("input[name=yes]:checked").val();
+//             if(valueadd==="yes"){
+//                 deliveryPrice= 500;
+//                 var getLocation = prompt("Tell your Location:");
+//                 alert("Will be delivered at" + getLocation);
+//             }
 
-        });
+//         });
 
-    $("form#pizzaform").submit(function(event){
+//     $("form#pizzaform").submit(function(event){
+//     var sizechoice=parsefloat(document.getElementById('pizzasize').value);
+//     var crustchoice=parsefloat(document.getElementById('pizzacrust').value);
+//     // var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
+
+//     var total= sizechoice + crustchoice + deliveryPrice;
+//     document.getElementById("totalamount").innerHTML =total;
+//     event.preventDefault();
+//     })
+
+// });
+
+
+
+
+
+
+
+
+
+var deliveryPrice=0;
+function pizza(){
+       
+     
+            // var valueadd =$("input[name=yes]:checked").val();
+            // if(valueadd==="yes"){
+            //     deliveryPrice= 500;
+            //     var getLocation = prompt("Tell your Location:");
+            //     alert("Will be delivered at" + getLocation);
+            // }
+
+        
+
+  
     var sizechoice=parsefloat(document.getElementById('pizzasize').value);
     var crustchoice=parsefloat(document.getElementById('pizzacrust').value);
     // var toppingschoice=parsefloat(document.getElementById('pizzatoppings').value);
 
     var total= sizechoice + crustchoice + deliveryPrice;
     document.getElementById("totalamount").innerHTML =total;
-    event.preventDefault();
-    })
+    // event.preventDefault();
+    
 
-});
+};
